4af34fb8bd85e74f2468cc5147ac930c8d4ae21f
pzabadala <p.zabadala@gmail.com>
2019-02-26 00:10:13 +0100

    ADC dry test

--- src/main.c
+++ src/main.c
@@ -8,35 +8,64 @@
   ******************************************************************************
 */
 
-
+#include <stdio.h>
+#include <stdint.h>
 #include "stm32f30x_rcc.h"
 #include "stm32f30x.h"
 #include "stm32f30x_gpio.h"
 
 
-void delay(int time)
+
+
+void send_char(char c)
 {
-    int i;
-    for (i = 0; i < time * 4000; i++) {}
+ while (USART_GetFlagStatus(USART2, USART_FLAG_TXE) == RESET);
+ USART_SendData(USART2, c);
+}
+
+int __io_putchar(int c)
+{
+ if (c=='\n')
+ send_char('\r');
+ send_char(c);
+ return c;
 }
 
 int main(void)
 {
     SystemInit();
-	GPIO_InitTypeDef gpio; // obiekt gpio z konfiguracja portow GPIO
-	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
+    ADC_InitTypeDef adc;
+	GPIO_InitTypeDef gpio;
+	USART_InitTypeDef uart;
 
-    GPIO_StructInit(&gpio); // domyslna konfiguracja
-    gpio.GPIO_Pin = GPIO_Pin_5; // konfigurujemy pin 5
-    gpio.GPIO_Mode = GPIO_Mode_OUT; // jako wyjscie
-    //gpio.GPIO_OType = GPIO_OType_PP;
-    //GPIO_Mode_Out_PP
-    GPIO_Init(GPIOA, &gpio); // inicjalizacja modulu GPIOA
+    /* ADC Initialization */
+    //RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
+    gpio.GPIO_Pin = GPIO_Pin_0;
+    gpio.GPIO_Mode = GPIO_Mode_AN;
+    GPIO_Init(GPIOA, &gpio);
+
+
+    USART_StructInit(&uart);
+    uart.USART_BaudRate = 115200;
+    USART_Init(USART2, &uart);
+    USART_Cmd(USART2, ENABLE);
+
+    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_ADC12, ENABLE);
+    RCC_ADCCLKConfig(RCC_ADC12PLLCLK_Div8);
+
+    ADC_StructInit(&adc);
+    adc.ADC_ContinuousConvMode = ENABLE;
+    adc.ADC_NbrOfRegChannel = 1;
+    ADC_Init(ADC1, &adc);
+    ADC_RegularChannelConfig(ADC1, ADC_Channel_1, 1, ADC_SampleTime_61Cycles5);
+    ADC_Cmd(ADC1, ENABLE);
+
+    ADC_StartConversion(ADC1);
 
     while (1) {
-        GPIO_SetBits(GPIOA, GPIO_Pin_5); // zapalenie diody
-        delay(100);
-        GPIO_ResetBits(GPIOA, GPIO_Pin_5); // zgaszenie diody
-        delay(400);
+    	uint16_t adc = ADC_GetConversionValue(ADC1);
+    	float v = (float)adc * 3.3f / 4096.0f;
+    	//printf("ADC = %d (%.3fV)\n", adc, v);
+
     }
 }
