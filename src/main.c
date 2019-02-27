abbf281196903ba49e01f4f97453b0b727aef5c9
pzabadala <p.zabadala@gmail.com>
2019-02-27 23:38:30 +0100

    Add Second ADC

--- src/main.c
+++ src/main.c
@@ -1,12 +1,12 @@
 /**
-  ******************************************************************************
-  * @file    main.c
-  * @author  Ac6
-  * @version V1.0
-  * @date    01-December-2013
-  * @brief   Default main function.
-  ******************************************************************************
-*/
+ ******************************************************************************
+ * @file    main.c
+ * @author  Piotr Z
+ * @version V1.0
+ * @date    20-Feb-2019
+ * @brief   Cooler main function.
+ ******************************************************************************
+ */
 
 #include <stdio.h>
 #include <stdint.h>
@@ -14,60 +14,57 @@
 #include "stm32f30x.h"
 #include "stm32f30x_gpio.h"
 
+	/**
+	 * PinOut -> https://raw.githubusercontent.com/wiki/RIOT-OS/RIOT/images/nucleo-f303_pinout.png
+	 * DataSheet -> https://www.st.com/resource/en/datasheet/stm32f303re.pdf
+	 * A6 -> PC0 -> ADC1 Channel 6
+	 * A5 -> PC1 -> ADC2 Channel 7
+	 *
+	 */
 
+int main(void) {
 
+	SystemInit();
+	ADC_InitTypeDef adc1;
+	ADC_InitTypeDef adc2;
 
-void send_char(char c)
-{
- while (USART_GetFlagStatus(USART2, USART_FLAG_TXE) == RESET);
- USART_SendData(USART2, c);
-}
-
-int __io_putchar(int c)
-{
- if (c=='\n')
- send_char('\r');
- send_char(c);
- return c;
-}
-
-int main(void)
-{
-    SystemInit();
-    ADC_InitTypeDef adc;
 	GPIO_InitTypeDef gpio;
 
-    /* ADC Initialization */
-    //RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
 	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOC, ENABLE);
-    gpio.GPIO_Pin = GPIO_Pin_0;
-    gpio.GPIO_Mode = GPIO_Mode_AN;
-    GPIO_Init(GPIOC, &gpio);
+	gpio.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1;
+	gpio.GPIO_Mode = GPIO_Mode_AN;
+	GPIO_Init(GPIOC, &gpio);
 
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_ADC12, ENABLE);
+	RCC_ADCCLKConfig(RCC_ADC12PLLCLK_Div10);
 
-    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_ADC12, ENABLE);
-    RCC_ADCCLKConfig(RCC_ADC12PLLCLK_Div10);
+	ADC_StructInit(&adc1);
+	adc1.ADC_ContinuousConvMode = ADC_ContinuousConvMode_Enable;
+	adc1.ADC_NbrOfRegChannel = 1;
+	adc1.ADC_ExternalTrigEventEdge = ADC_ExternalTrigEventEdge_None;
+	adc1.ADC_Resolution = ADC_Resolution_12b;
+	ADC_Init(ADC1, &adc1);
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_6, 1, ADC_SampleTime_1Cycles5);
+	ADC_Cmd(ADC1, ENABLE);
 
-    ADC_StructInit(&adc);
-    adc.ADC_ContinuousConvMode = ADC_ContinuousConvMode_Enable;
-    adc.ADC_NbrOfRegChannel = 1;
-    adc.ADC_ExternalTrigEventEdge = ADC_ExternalTrigEventEdge_None;
-    adc.ADC_Resolution = ADC_Resolution_12b;
-    ADC_Init(ADC1, &adc);
-    ADC_RegularChannelConfig(ADC1, ADC_Channel_6, 1, ADC_SampleTime_1Cycles5);
+	ADC_StructInit(&adc2);
+	adc2.ADC_ContinuousConvMode = ADC_ContinuousConvMode_Enable;
+	adc2.ADC_NbrOfRegChannel = 1;
+	adc2.ADC_ExternalTrigEventEdge = ADC_ExternalTrigEventEdge_None;
+	adc2.ADC_Resolution = ADC_Resolution_12b;
+	ADC_Init(ADC2, &adc2);
+	ADC_RegularChannelConfig(ADC2, ADC_Channel_7, 1, ADC_SampleTime_1Cycles5);
+	ADC_Cmd(ADC2, ENABLE);
 
-    ADC_Cmd(ADC1, ENABLE);
+	while (!((ADC_GetFlagStatus(ADC1, ADC_FLAG_RDY)&ADC_GetFlagStatus(ADC2, ADC_FLAG_RDY))));
+	ADC_StartConversion(ADC1);
+	ADC_StartConversion(ADC2);
 
-    while(!(ADC_GetFlagStatus(ADC1, ADC_FLAG_RDY)));
-    ADC_StartConversion(ADC1);
-    //while(ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC) == RESET){}
-    float v;
-    uint16_t dupa;
+	uint16_t voltage1;
+	uint16_t voltage2;
 
 	while (1) {
-    	dupa = ADC_GetConversionValue(ADC1);
-    	v = (float)dupa * 3.3f / 4096.0f;
-
-
-    }
+		voltage1 = ADC_GetConversionValue(ADC1);
+		voltage2 = ADC_GetConversionValue(ADC2);
+	}
 }
